f0835683ae8ff5ad3fea8552e594e8781b2c91a3
Ali Arshadi <realgabral@protonmail.com>
2024-12-15 12:14:03 +0330

    done

---
+++ pyramid_star/pyramid_star.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char star = 42;
+    char space = 32;
+
+    int givenNumber;
+    cout << "Please enter a number \n";
+    cin >> givenNumber;
+    // *upper half of the pyramid
+    for (int i = 1; i <= givenNumber; i++)
+    {
+        // * this is a for loop for space
+        for (int j = 1; j <= givenNumber - i; j++)
+        {
+            cout << space;
+        }
+        // * this is for loop for stars
+        for (int k = 1; k <= givenNumber - (givenNumber - i); k++)
+        {
+            cout << star << space;
+        }
+
+        cout << "\n";
+    }
+    //* lower half of the pyramid
+    for (int i = 1; i <= givenNumber - 1; i++)
+    {
+        // * this is a for loop for space
+        for (int j = 1; j <= givenNumber - (givenNumber - i); j++)
+        {
+            cout << space;
+        }
+
+        // * this is a for loop for stars
+        for (int k = 1; k <= givenNumber - i; k++)
+        {
+            cout << star << space;
+        }
+
+        cout << "\n";
+    }
+}
