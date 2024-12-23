42968774d23e82ee62412c9ba8b6b9bee1612e17
Ali Arshadi <realgabral@protonmail.com>
2024-12-23 15:16:40 +0330

    isPrime function written

---
+++ get-prime-numbers/get_prime_numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+//* the function checks if a number is prime!
+bool isPrime(int n)
+{
+    if (n == 1)
+    {
+        return false;
+    }
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+}
