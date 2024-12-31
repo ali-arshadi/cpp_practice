afcfe2b8a012c8e6f2977adc17a1ede192a1b7c9
Ali Arshadi <realgabral@protonmail.com>
2024-12-31 14:09:34 +0330

    moving isPrime fucntion to entities

--- get-prime-numbers-array-approche/prime_numbers_array.cpp
+++ get-prime-numbers-array-approche/prime_numbers_array.cpp
@@ -1,20 +1,6 @@
 #include <iostream>
+#include "./entities/isPrime.cpp"
 using namespace std;
-bool isPrime(int n)
-{
-    if (n == 1)
-    {
-        return false;
-    }
-    for (int i = 2; i < n; i++)
-    {
-        if (n % i == 0)
-        {
-            return false;
-        }
-    }
-    return true;
-}
 
 int main()
 {
